367ee3d4dc55baf94b9bd967c2b1bac8689cbccc
whg420 <55119058+whg420@users.noreply.github.com>
2020-01-06 14:10:29 +0800

    321

---
+++ my-app/src/router/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import {BrowserRouter,Switch,Route,Redirect} from 'react-router-dom';
+class index extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default index;
---
+++ my-app/src/router/routerViews.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class routerViews extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default routerViews;
